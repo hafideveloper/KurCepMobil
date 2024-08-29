0f2c51c4533b566d64416258672dd5f502de5179
hafideveloper <hafideveloperr@gmail.com>
2024-08-29 10:20:54 +0300

    The corners of the cell are rounded, a thin gray border is added to the edges

--- KurCepMobil/HomeViewController.swift
+++ KurCepMobil/HomeViewController.swift
@@ -33,6 +33,11 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
             cell.toLabel.text = "TRY"
             cell.changeRateLabel.text = String(currencyList[indexPath.row].changeRate)
             cell.priceLabel.text = String(currencyList[indexPath.row].price)
+        
+        cell.contentView.layer.cornerRadius = 5
+        cell.contentView.layer.masksToBounds = true
+        cell.contentView.layer.borderWidth = 0.5
+        cell.contentView.layer.borderColor = UIColor.lightGray.cgColor
             return cell
         }
 
